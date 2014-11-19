570373de897376db8a4810b49ce5cd6443226bb1
Dean-Christian-Armada <deanarmada@gmail.com>
2014-11-19 13:43:09 +0800

    Revert "Revert "Revert "Revert "Remove all""""
    This reverts commit dd317033e35da2fc0e236ce049cb6a7f8227d3bf.

--- page_name.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$path = $_SERVER['PHP_SELF']; 
-	$page = basename($path); 
-	$page = basename($path, '.php');
-	echo $page;
-?>
--- str_replace.php
+++
@@ -1,6 +0,0 @@
-<?php
-$name = 'dean_christian_armada';
-$name = str_replace('_', ' ', $name);
-$name = ucfirst($name);
-echo $name;
-?>
